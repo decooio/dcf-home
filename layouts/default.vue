190dc3dea5bb8ea8e2d448e0eaf48a0f66b78e82
fanxiaoyi <fanxiaoyi1995@126.com>
2021-04-02 14:50:51 +0800

    auto adjust zoom for small screen

--- layouts/default.vue
+++ layouts/default.vue
@@ -48,39 +48,14 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 
 nav-bar {
   position: absolute;
 }
 
+@media only screen and (max-width: 1440px) {
+  body { zoom: 0.75}
+}
 @media only screen and (max-width: 640px) { html, body { font-size: 32px; } }
 /* for 540 px width screen */
 @media only screen and (max-width: 540px) { html, body { font-size: 27px; } }
